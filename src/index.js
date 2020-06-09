253c24f881964c12a710a8ecd2cd2b9be0955a5f
Сергей Харлов <l1nks@inbox.ru>
2020-06-09 16:40:26 +0500

    базовая разметка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
+const filmInformation = {
+  date: 2014,
+  genre: `Drama`
+};
+
+ReactDOM.render(<App data={filmInformation} />, document.querySelector(`#root`));
