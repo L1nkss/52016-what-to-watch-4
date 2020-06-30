028f9c8a22f796c47358870f896557fa04e32523
Сергей Харлов <l1nks@inbox.ru>
2020-06-30 12:36:53 +0500

    Обновил тесты

--- src/components/film-card/film-card.e2e.test.jsx
+++ src/components/film-card/film-card.e2e.test.jsx
@@ -9,12 +9,19 @@ const film = {
 
 const renderPlayerMock = jest.fn();
 const clickHandlerMock = jest.fn();
+const callback = jest.fn();
 
 describe(`Testing e2e FilmCard component`, () => {
   it(`Should be clicked on title`, () => {
     const onTitleClickHandler = jest.fn();
     const Component = shallow(
-        <FilmCard filmInfo={film} renderPlayer={renderPlayerMock} onClickCardHandler={onTitleClickHandler}/>
+        <FilmCard
+          filmInfo={film}
+          renderPlayer={renderPlayerMock}
+          onClickCardHandler={onTitleClickHandler}
+          handleMouseEnter={callback}
+          handleMouseLeave={callback}
+        />
     );
     const Title = Component.find(`.small-movie-card__link`);
     Title.simulate(`click`);
@@ -24,7 +31,13 @@ describe(`Testing e2e FilmCard component`, () => {
   it(`Should be clicked on image of film`, () => {
     const onImageClickHandler = jest.fn();
     const Component = shallow(
-        <FilmCard filmInfo={film} renderPlayer={renderPlayerMock} onClickCardHandler={onImageClickHandler}/>
+        <FilmCard
+          filmInfo={film}
+          renderPlayer={renderPlayerMock}
+          onClickCardHandler={onImageClickHandler}
+          handleMouseEnter={callback}
+          handleMouseLeave={callback}
+        />
     );
     const Title = Component.find(`.small-movie-card__image`);
     Title.simulate(`click`);
@@ -34,7 +47,13 @@ describe(`Testing e2e FilmCard component`, () => {
   it(`Should return true when mouseEnter`, () => {
     const onCardMouseEnter = jest.fn(() => true);
     const Component = shallow(
-        <FilmCard filmInfo={film} renderPlayer={renderPlayerMock} onClickCardHandler={clickHandlerMock} handleMouseEnter={onCardMouseEnter}/>
+        <FilmCard
+          filmInfo={film}
+          renderPlayer={renderPlayerMock}
+          onClickCardHandler={clickHandlerMock}
+          handleMouseEnter={onCardMouseEnter}
+          handleMouseLeave={callback}
+        />
     );
     const Article = Component.find(`.small-movie-card`);
     Article.simulate(`mouseover`);
@@ -43,7 +62,13 @@ describe(`Testing e2e FilmCard component`, () => {
   it(`Should return false when mouseLeave`, () => {
     const onCardMouseLeave = jest.fn(() => false);
     const Component = shallow(
-        <FilmCard filmInfo={film} renderPlayer={renderPlayerMock} onClickCardHandler={clickHandlerMock} handleMouseLeave={onCardMouseLeave}/>
+        <FilmCard
+          filmInfo={film}
+          renderPlayer={renderPlayerMock}
+          onClickCardHandler={clickHandlerMock}
+          handleMouseLeave={onCardMouseLeave}
+          handleMouseEnter={callback}
+        />
     );
     Component.simulate(`mouseleave`);
     expect(onCardMouseLeave.mock.calls.length).toBe(1);
