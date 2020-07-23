5bcad97a70ed82f84a433cb570711989d69a7ec6
Сергей Харлов <l1nks@inbox.ru>
2020-07-24 01:29:08 +0500

    Добавлен переход на страницу My-list + переход на главную

--- src/components/my-list/my-list.jsx
+++ src/components/my-list/my-list.jsx
@@ -1,24 +1,12 @@
+import Header from "@components/header/header.connect";
+
 export default class MyList extends React.Component {
   render() {
     return (
       <div className="user-page">
-        <header className="page-header user-page__head">
-          <div className="logo">
-            <a href="main.html" className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
+        <Header isUserPage={true}>
           <h1 className="page-title user-page__title">My list</h1>
-
-          <div className="user-block">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-            </div>
-          </div>
-        </header>
+        </Header>
 
         <section className="catalog">
           <h2 className="catalog__title visually-hidden">CatalogNav</h2>
