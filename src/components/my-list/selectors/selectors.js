3ac78e395e19a90bae25baa76aeaf2d94326a017
Сергей Харлов <l1nks@inbox.ru>
2020-07-24 10:10:57 +0500

    поправил тесты

--- src/components/my-list/selectors/selectors.js
+++ src/components/my-list/selectors/selectors.js
@@ -1,3 +1,1 @@
-import {createSelector} from "reselect";
-
 export const getFavoriteFilms = (state) => state.DATA.films.filter((film) => film.isFavorite);
--- src/reducer/promo/promo.test.js
+++ src/reducer/promo/promo.test.js
@@ -4,30 +4,30 @@ import ActionType from "./constants/constants.js";
 describe(`Promo reducer test`, () => {
   it(`Should return initial state`, () => {
     expect(reducer(undefined, {})).toEqual({
-      film: null,
+      film: {},
       loading: false,
       error: false
     });
   });
   it(`Should change promo status correctly depends on inbox values`, () => {
-    expect(reducer({film: null, loading: false, error: false}, {
+    expect(reducer({film: {}, loading: false, error: false}, {
       type: ActionType.LOAD_PROMO_FILM_REQUEST
     })).toEqual({
-      film: null, loading: true, error: false
+      film: {}, loading: true, error: false
     });
-    expect(reducer({film: null, loading: false, error: false}, {
+    expect(reducer({film: {}, loading: false, error: false}, {
       type: ActionType.LOAD_PROMO_FILM_SUCCESS,
       payload: []
     })).toEqual({
       film: [], loading: false, error: false
     });
-    expect(reducer({film: null, loading: false, error: false}, {
+    expect(reducer({film: {}, loading: false, error: false}, {
       type: ActionType.LOAD_PROMO_FILM_SUCCESS,
       payload: [{name: `Legend`}]
     })).toEqual({
       film: [{name: `Legend`}], loading: false, error: false
     });
-    expect(reducer({film: null, loading: false, error: false}, {
+    expect(reducer({film: {}, loading: false, error: false}, {
       type: ActionType.LOAD_PROMO_FILM_SUCCESS,
       payload: [{name: `Legend`, genre: `Crime`}, {name: `Snatch`, genre: `Comedy`}]
     })).toEqual({
